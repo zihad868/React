c2dc1612fa85e121a9376db145bb740fb0abc55e
zihad868 <zihadh868@gmail.com>
2023-11-19 19:23:15 +0600

    Navbar & Pages Creation

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 
+import {Link} from 'react-router-dom'
+
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("shop");
+
   return (
+
+    
+
     <div className='navbar'>
+
+      <div className="nav-logo">
+        <img src={logo}/>
+        <p>Shop</p>
+      </div>
       
+      <ul className="nav-menu">
+        <li onClick={() => {setMenu("shop")}}> <Link style={{textDecoration: 'none'}} to='/'>Shop</Link> {menu === "shop" ? <hr /> : <></>} </li>
+        <li onClick={() => {setMenu("mens")}}> <Link style={{textDecoration: 'none'}} to='/mens'>Mens</Link> {menu === "mens" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("womens")}}> <Link style={{textDecoration: 'none'}} to='/womens'>Womens</Link> {menu === "womens" ? <hr /> : <></>} </li>
+        <li onClick={() => {setMenu("kids")}}> <Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link> {menu === "kids" ? <hr /> : <></>} </li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <Link style={{textDecoration: 'none'}} to='/login'><button>Login</button></Link>
+        <Link style={{textDecoration: 'none'}} to='/cart'><img src={cart_icon} /></Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+
     </div>
   )
 }
