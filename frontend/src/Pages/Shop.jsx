f00e50f928c9aabadd6ee33627bf0a2b9128a3ca
zihad868 <zihadh868@gmail.com>
2023-11-20 11:27:49 +0600

    Product Add

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      <Hero />
+      <Popular />
+    </div>
   )
 }
 
